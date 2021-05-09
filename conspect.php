5e2d37174af1e63229e7fddca1b81a689d242f16
mihailk111 <you@example.com>
2021-05-10 01:58:09 +0500

    "update"

---
+++ conspect.php
@@ -0,0 +1,58 @@
+<?php
+
+
+
+
+/**
+ ** Standart url reges
+ * 
+ */
+
+$urlRegex = "/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i";
+
+
+
+
+
+/**
+ *  Dump and Die
+ *
+ */
+
+function dd($data)
+{
+  var_dump($data);
+  die;
+}
+
+
+/*
+class Queue
+{
+    private $data = [];
+    public function __construct() { }
+    public function put($value) { array_push($this->data, $value); }
+    public function get() { return array_shift($this->data); }
+    public function forEach(callable $callback) { if (count($this->data) === 0) return; while($data = $this->get()) { $callback($data); } }
+
+}
+*/
+
+
+ 
+/**
+ *  Standart http headers
+ */
+
+$http_request = <<< HEADERS
+    GET /home HTTP/1.1
+    Host: twitter.com
+    User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0
+    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+    Accept-Language: en-US,en;q=0.5
+    Cookie: data=somethiashdg; PHPSESSID=j237vb123612v36v12735123v727344444444444294;
+    Accept-Encoding: gzip, deflate, brk
+    Connection: keep-alive\r\n\r\n
+HEADERS;
+
+
---
+++ time.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace MK11;
+
+class exTime
+{
+    private $time;
+    private $wasEnded = false;
+
+    public function start()
+    {
+       $this->time = hrtime(true); 
+    } 
+
+    public function end()
+    {
+        $this->wasEnded = true;
+        $this->time = hrtime(true) - $this->time; 
+    }
+
+    /**
+     * 
+     *   Time in seconds
+     *   
+     */
+    public function seconds(): string
+    {
+        $this->checkIfEnded();
+        return strval($this->time/1000000000); 
+    }
+
+    /**
+     *  Time in milliseconds
+     */
+    public function time() : string
+    {
+        $this->checkIfEnded();
+        return strval($this->time/1000000); 
+    }
+
+    /**
+     *   Time in nanoseconds
+     */
+    public function nano(): string
+    {
+        $this->checkIfEnded();
+        return strval($this->time); 
+    }
+
+
+    private function checkIfEnded()
+    {
+        if (!$this->wasEnded) throw new Exception("end() method was not called");
+    }
+    
+}
