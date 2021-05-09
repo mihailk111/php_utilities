0db66ea0a954fb478a184637e058cacc875ea6a9
mihailk111 <you@example.com>
2021-05-10 00:55:58 +0500

    soomething

--- Utilities.php
+++ Utilities.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Utilities;
+namespace MK11;
 
 /**
  *  Creates file if it does not exist
  */
+
 function touchFile(string $filePath): void
 {
     if (!file_exists($filePath))
@@ -13,40 +14,54 @@ function touchFile(string $filePath): void
     }
 }
 
+/**
+ ** Standart url reges
+ * 
+ */
+// $urlRegex = "/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i";
 
 
+
+
+
+/**
+ *  Dump and Die
+ *
+ */
+function dd($data)
+{
+  var_dump($data);
+  die;
+}
 
 
 /*
 class Queue
 {
     private $data = [];
-    private $types = ['int', 'string', 'object'];
-
-    public function __construct()
-    {
-         
-    }
-    public function put($value)
-    {
-        array_push($this->data, $value);
-    }
-    public function get()
-    {
-        return array_shift($this->data); 
-    }
-
-    public function forEach(callable $callback)
-    {
-
-        if (count($this->data) === 0) return;
-
-        while($data = $this->get())
-        {
-            $callback($data);
-        }
-    }
+    public function __construct() { }
+    public function put($value) { array_push($this->data, $value); }
+    public function get() { return array_shift($this->data); }
+    public function forEach(callable $callback) { if (count($this->data) === 0) return; while($data = $this->get()) { $callback($data); } }
 
 }
 */
+
+
+ 
+/**
+ *  Standart http headers
+ */
+
+// $http_request = <<< HEADERS
+//     GET /home HTTP/1.1
+//     Host: twitter.com
+//     User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0
+//     Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+//     Accept-Language: en-US,en;q=0.5
+//     Cookie: data=somethiashdg; PHPSESSID=j237vb123612v36v12735123v727344444444444294;
+//     Accept-Encoding: gzip, deflate, brk
+//     Connection: keep-alive\r\n\r\n
+// HEADERS;
+
 
